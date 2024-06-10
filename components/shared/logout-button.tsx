355f84c6c16f72dd5007a5251cdb5504aa9b4ba1
sandy <sandeshjadhav2002@gmail.com>
2024-06-10 21:05:12 +0530

    useFormStatus & useFormState

--- components/shared/logout-button.tsx
+++ components/shared/logout-button.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import { Button } from "../ui/button";
 import { LogOut } from "lucide-react";
 import { signOut } from "@/auth";
+import { logoutAction } from "@/lib/actions";
 
 const LogoutButton = () => {
-  async function logoutAction() {
-    "use server";
-    await signOut();
-  }
+
   return (
     <form action={logoutAction}>
       <Button className="bg-black text-white rounded-full p-3 text-xs md:text-sm">
