8712e17afb8d19c0c1e8335a53f10a30e373a5f4
sandy <sandeshjadhav2002@gmail.com>
2024-06-15 01:36:58 +0530

    user.id type check fix

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,44 +1,51 @@
-import mongoose, { Document, Model } from "mongoose";
+import mongoose from "mongoose";
+import { Document, Model } from "mongoose";
 
-export interface IUSer {
-  username: string;
-  fullName: string;
-  email: string;
-  avatar?: string;
+
+
+export interface IUser {
+	username: string;
+	fullName: string;
+	email: string;
+	avatar?: string;
 }
 
-export interface IUserDocument extends IUSer, Document{
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-const userSchema = new mongoose.Schema<IUserDocument>(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    fullName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    avatar: {
-      type: String,
-      default: "",
-    },
-  },
-  {
-    timestamps: true,
+export interface IUserDocument extends IUser, Document {
+	_id: mongoose.Schema.Types.ObjectId;
+	username: string;
+	fullName: string;
+	email: string;
+	avatar?: string;
+	createdAt: Date;
+	updatedAt: Date;
   }
+const userSchema = new mongoose.Schema<IUserDocument>(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		fullName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		avatar: {
+			type: String,
+			default: "",
+		},
+	},
+	{
+		// createdAt, updatedAt
+		timestamps: true,
+	}
 );
 
-const User: Model<IUserDocument> =
-  mongoose.models?.User || mongoose.model("User", userSchema);
+const User: Model<IUserDocument> = mongoose.models?.User || mongoose.model("User", userSchema);
 
 export default User;
